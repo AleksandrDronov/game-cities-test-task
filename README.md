2fc318453d42998809f7cce487b2a93e058432b1
Aleksandr Dronov <dronovspb@gmail.com>
2023-11-15 06:36:49 +0300

    docs(a): add readmy

---
+++ README.md
@@ -0,0 +1,25 @@
+# Тестовое задание. Игра "города"
+
+[деплой](https://game-cities-test-task-ml71.vercel.app/)
+
+### Используемые технологии:
+1. NextJS
+2. TypeScript
+
+### Запуск проекта:
+
+1. Клонировать репозиторий.
+
+   git clone git@github.com:AleksandrDronov/game-cities-test-task.git
+
+2. Войти в папку проекта.
+
+   cd game-cities-test-task
+
+3. Установить зависимости.
+
+   npm install
+
+4. Запуск.
+
+   npm run dev
--- app/firstcity/page.tsx
+++ app/firstcity/page.tsx
@@ -5,6 +5,7 @@ import getCity, { data } from "@/libs/get-city";
 import reduceValue from "@/libs/reduce-value";
 import { FormEvent, useEffect, useRef, useState } from "react";
 import Countdown from "react-countdown";
+import Icon from "@/components/icon";
 
 const renderer = ({
   minutes,
@@ -28,7 +29,6 @@ export default function FirstCityPage() {
   const [isFirstSubmit, setIsFirstSubmit] = useState(true);
   const [countdownOver, setCountdownOver] = useState(false);
   const ref = useRef<null | HTMLLIElement>(null);
-
   const [error, setError] = useState("");
 
   const lastCity = cities[cities.length - 1];
@@ -163,35 +163,7 @@ export default function FirstCityPage() {
             className="bg-violet-600 text-white p-[6px] rounded disabled:bg-gray-400 hover:opacity-75 transition-all"
             disabled={!isYouTurn || !value}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-            >
-              <g clipPath="url(#clip0_7618_580)">
-                <path
-                  d="M8.33337 11.6667L17.5 2.5"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M17.5001 2.5L12.0834 17.5C12.0468 17.5798 11.9881 17.6474 11.9143 17.6948C11.8404 17.7422 11.7545 17.7674 11.6667 17.7674C11.579 17.7674 11.493 17.7422 11.4192 17.6948C11.3453 17.6474 11.2866 17.5798 11.2501 17.5L8.33339 11.6667L2.50006 8.75C2.42027 8.71344 2.35266 8.65474 2.30526 8.58088C2.25786 8.50701 2.23267 8.4211 2.23267 8.33333C2.23267 8.24557 2.25786 8.15965 2.30526 8.08579C2.35266 8.01193 2.42027 7.95323 2.50006 7.91667L17.5001 2.5Z"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_7618_580">
-                  <rect width="20" height="20" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
+            <Icon />
           </button>
         </div>
       </form>
